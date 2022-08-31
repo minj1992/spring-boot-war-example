0324caf8196503b3aaca1d82fed11598310540f0
minj1992 <57328135+minj1992@users.noreply.github.com>
2022-08-31 10:30:20 -0700

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. this is from nexus";
+        return "This is from GHOSTlearner classes";
     }
 
 }
